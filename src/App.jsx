9cec54a188bd60d4c5a6905e0bc95f18fb2f6717
Subinmhrzn <subinmhrzn079@gmail.com>
2023-10-17 15:30:49 +0545

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Sigin from "./pages/Sigin";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/Signup";
+import Header from "./components/Header";
 
 
 
 export default function App() {
   return(
   <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/sign-in" element={<Sigin/>}/>
